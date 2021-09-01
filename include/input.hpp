6472064215f4eecf58c8bd3a611ffa081081ce80
igorlfs <igorlfs@ufmg.br>
2021-09-01 20:55:54 -0300

    refactor: play() -> readColumn() + main loop
    readColumn(): handle exceptions
    readPlayer(): don't bother throwing a special exception for a
    blank line; use constants

--- include/input.hpp
+++ include/input.hpp
@@ -4,8 +4,11 @@
 namespace Input {
 struct interrupt {};
 struct invalidColorFormat {};
+struct invalidNumberFormat {};
 struct invalidColor {
     std::string str;
 };
+struct numberOutOfBounds {};
+struct fullColumn {};
 } // namespace Input
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 char readPlayer();
-void play(char *jogador, Board::grid tabuleiro);
+uint readColumn(char jogador, Board::grid tabuleiro);
 
 int main() {
     Board::grid grid;
@@ -28,24 +28,19 @@ chooseColorAgain:
         cout << "Escolha sua cor ('A' para Amarelo ou 'V' para Vermelho): ";
         std::getline(std::cin, readLine);
         if (std::cin.eof()) throw Input::interrupt();
-        if (readLine.empty()) throw Input::emptyLine();
         if (!std::regex_match(readLine, expectedFormat))
-            throw Input::invalidColorFormat{readLine};
+            throw Input::invalidColorFormat();
         char colorInitial;
         std::stringstream ss(readLine);
         ss >> colorInitial;
-        if (colorInitial != 'A' && colorInitial != 'V')
+        if (colorInitial != CHAR_YELLOW && colorInitial != CHAR_RED)
             throw Input::invalidColor{readLine};
         return colorInitial;
     } catch (Input::interrupt e) {
         std::cout << "\n\nA entrada de dados foi interrompida. Saindo.\n\n";
         exit(1);
-    } catch (Input::emptyLine e) {
-        std::cout << "\nNão foi informado nenhum dado."
-                  << "\nPor favor, insira uma cor.\n\n";
-        goto chooseColorAgain;
     } catch (Input::invalidColorFormat e) {
-        std::cout << "\nEntrada inválida: " << e.str
+        std::cout << "\nEntrada inválida."
                   << "\nA cor deve ser uma ÚNICA letra minúscula, a inicial da "
                      "cor desejada\n\n";
         goto chooseColorAgain;
@@ -56,30 +51,40 @@ chooseColorAgain:
     }
 }
 
-void play(char *player, Board::grid grid) {
-    int numMoves = 0;
-
-    while (numMoves < Board::grid::MAX_JOGADAS) {
-        int currentPlayer = numMoves % 2;
-        cout << "Vez do jogador " << currentPlayer + 1 << " ";
-        (player[currentPlayer] == CHAR_YELLOW)
-            ? cout << '(' << YELLOW << CHAR_YELLOW << RESET << ')'
-            : cout << '(' << RED << CHAR_RED << RESET << ')';
-        cout << endl << "Escolha uma coluna: ";
-        string input;
-        int jogada;
-        getline(cin, input);
-        jogada = stoi(input);
-        if (grid.checkUpdateArena(jogada - 1) && cin.good()) {
-            grid.updateArena(jogada - 1, player[currentPlayer]);
-            grid.printArena();
-            if (grid.checkWin(player[currentPlayer])) {
-                cout << currentPlayer + 1 << " venceu!\n";
-                return;
-            }
-            numMoves++;
-            if (numMoves == Board::grid::MAX_JOGADAS)
-                cout << "O jogo empatou!" << endl;
-        }
+uint readColumn(char player, Board::grid grid) {
+chooseNumberAgain:
+    try {
+        std::regex expectedFormat("\\s*\\d+\\s*");
+        std::string readLine;
+        cout << "\nEscolha uma coluna: ";
+        std::getline(std::cin, readLine);
+        if (std::cin.eof()) throw Input::interrupt();
+        if (!std::regex_match(readLine, expectedFormat))
+            throw Input::invalidNumberFormat();
+        std::stringstream ss(readLine);
+        uint column;
+        ss >> column;
+        if (column == 0) throw Input::numberOutOfBounds();
+        column--;
+        if ((column > Board::grid::COLS - 1)) throw Input::numberOutOfBounds();
+        if (grid.getCell(0, column) != Board::EMPTY) throw Input::fullColumn();
+        return column;
+    } catch (Input::interrupt e) {
+        std::cout << "\n\nA entrada de dados foi interrompida. Saindo.\n\n";
+        exit(1);
+    } catch (Input::invalidNumberFormat e) {
+        std::cout << "\nEntrada inválida."
+                  << "\nVocê deve escolher um ÚNICO NÚMERO natural\n";
+        goto chooseNumberAgain;
+    } catch (Input::numberOutOfBounds e) {
+        std::cout << "Você inseriu um número inválido!\n"
+                     "Insira um número de 1 a "
+                  << Board::grid::COLS << ".\n";
+        goto chooseNumberAgain;
+    } catch (Input::fullColumn e) {
+        std::cout << "Jogada Inválida.\n"
+                     "Você tentou jogar numa coluna cheia!\n"
+                     "Escolha outra coluna.\n";
+        goto chooseNumberAgain;
     }
 }
